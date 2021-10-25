77d9f82e056f554e1a7b1bb98ec7a1b4751296a0
sarasaud <unix.663@gmail.com>
2021-10-25 15:49:25 +0300

    done22

---
+++ 22dark/22dark/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  22dark
+//
+//  Created by SARA SAUD on 3/19/1443 AH.
+//
+
+
+import UIKit
+
+
+    struct Post: Codable {
+        let name: String
+        let gender: String
+        let probability: Double
+        let count: Int
+    }
+class ViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        simpleGetUrlRequset()
+    //dark Mood
+        //Gesture run func when tapped
+        //1
+        let Up = UISwipeGestureRecognizer(
+            target: self,
+            action: #selector(backGroundColor))
+        
+        Up.direction = .up
+        view.addGestureRecognizer(Up)
+        
+        
+        view.isUserInteractionEnabled = true
+
+         
+////        2
+        let tap = UITapGestureRecognizer(
+            target: self,
+            action: #selector(backGroundColor1))
+        tap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tap)
+
+        view.isUserInteractionEnabled = true
+   }
+     @objc func backGroundColor(){
+       view.backgroundColor = UIColor(named: "BackGround")
+       
+   }
+    @objc func backGroundColor1(){
+        view.backgroundColor = UIColor(named: "BackGround2")
+
+    }
+
+    @IBOutlet weak var name1: UILabel!
+    @IBOutlet weak var gender1: UILabel!
+    @IBOutlet weak var probablitity1: UILabel!
+    @IBOutlet weak var count1: UILabel!
+    
+      @objc  func simpleGetUrlRequset(){
+            let url = URL(string: "https://api.genderize.io/?name=luc")!
+            let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                guard let data = data else {return}
+                print(String(data: data , encoding: .utf8)!)
+             let posts = try? JSONDecoder().decode(Post.self, from: data)
+
+                DispatchQueue.main.async {
+                    self.name1.text = posts?.name
+                    self.gender1.text = posts?.gender
+                    self.probablitity1.text = "\(posts!.probability)"
+                    self.count1.text = "\(posts!.count)"
+                    
+                }
+            }
+            task.resume()
+        }
+}
